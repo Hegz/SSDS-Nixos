0e03d967766b927371e841867e26c901d7407ee7
Adam Fairbrother <adam.fairbrother@gmail.com>
2023-10-30 13:19:44 -0700

    Fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,5 @@ sudo ln -s /home/dbert/SSDS-Nixos/configuration.nix /etc/nixos/configuration.nix
 sudo rm /etc/nixos/hardware-configuration.nix
 sudo ln -s /home/dbert/SSDS-Nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
 
-echo Precreating secrets
-sudo mkdir /run/secrets/
-sudo touch /run/secrets/ssh_key
-
 echo Deploying system
 sudo nixos-rebuild switch
