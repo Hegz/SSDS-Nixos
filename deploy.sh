ed558676033c27ba4b8c48585f43d53fc28eb7e6
Adam Fairbrother <adam.fairbrother@gmail.com>
2023-10-30 11:14:53 -0700

    Precreate some secrets to avoid file not found errors.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
+
+echo Linking configuration files in to place
 sudo rm /etc/nixos/configuration.nix
 sudo ln -s /home/dbert/SSDS-Nixos/configuration.nix /etc/nixos/configuration.nix
 
 sudo rm /etc/nixos/hardware-configuration.nix
 sudo ln -s /home/dbert/SSDS-Nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
 
+echo Precreating secrets
+sudo mkdir /run/secrets/
+sudo touch /run/secrets/ssh_key
+
+echo Deploying system
 sudo nixos-rebuild switch
