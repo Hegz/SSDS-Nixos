4d19d70ec7cf6fbc32900767c292d34093bca96e
Adam Fairbrother <adam.fairbrother@gmail.com>
2023-11-06 10:09:06 -0800

    Dir fix

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 echo Linking configuration files in to place
 sudo rm /etc/nixos/configuration.nix
-sudo ln -s $(pwd)/SSDS-Nixos/configuration.nix /etc/nixos/configuration.nix
+sudo ln -s $(pwd)/configuration.nix /etc/nixos/configuration.nix
 
 sudo rm /etc/nixos/hardware-configuration.nix
-sudo ln -s $(pwd)/SSDS-Nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
+sudo ln -s $(pwd)/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
 
 echo Deploying system
 sudo nixos-rebuild switch
