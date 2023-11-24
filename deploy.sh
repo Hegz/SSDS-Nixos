837fdbb7c5707d337133463385e0ff91c4cb980b
Adam Fairbrother <adam.fairbrother@gmail.com>
2023-11-24 13:48:38 -0800

    Stop libreoffice before system upgradewq

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
+echo Pulling latest changes
 git pull
 
 echo Linking configuration files in to place
 sudo rm /etc/nixos/configuration.nix
 sudo ln -s $(pwd)/configuration.nix /etc/nixos/configuration.nix
 
+echo Linking hardware configuration files in to place
 sudo rm /etc/nixos/hardware-configuration.nix
 sudo ln -s $(pwd)/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
+
+echo Killing LibreOffice
+killall soffice.bin
 
 echo Deploying system
 sudo nixos-rebuild switch
