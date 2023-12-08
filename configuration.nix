16aeb8c0738efbe257e311d3bbdd3d12d62a14c9
Adam Fairbrother <adam.fairbrother@gmail.com>
2023-12-08 09:31:50 -0800

    Sent control signal when libreoffce force killed.

--- configuration.nix
+++ configuration.nix
@@ -187,6 +187,7 @@ in
           Type = "oneshot";
           ExecStart = toString ( pkgs.writeShellScript "soffice_refresh.sh" ''
             ${pkgs.killall}/bin/killall soffice.bin
+            ${pkgs.touch}/bin/touch /home/otto/Control/End
           '');
         };
       };
