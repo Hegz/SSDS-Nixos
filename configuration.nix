dcd19563b1f1f500ca0dd24d8558d7425e94193d
Adam Fairbrother <adam.fairbrother@gmail.com>
2023-12-28 11:10:28 -0800

    Pinned home manager release to 23.05

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,7 @@ let
   # Add the home manager channel to the system 
   home-manager = builtins.fetchTarball {
     url = "https://github.com/nix-community/home-manager/archive/33110fb3c7fe6a94b98b641866a5eddb64b7c23f.tar.gz";
-    sha256 = "sha256:1yy95p3bwmpif6ld9dqw90gpfjn87frn36g0qp2fplpj38qxiab1";
+    sha256 = "";
     # Pinned to release date 28/Dec/2023
   }; 
   # Raspberry pi harware overlays
