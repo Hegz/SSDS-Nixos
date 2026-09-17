2370c317facdd4cb40d0adef388cdd62feeb222f
Adam Fairbrother <adam.fairbrother@gmail.com>
2026-09-17 13:01:33 -0700

    restart main script on update.

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,6 @@ sudo killall soffice.bin || true
 
 echo Deploying system
 sudo nixos-rebuild switch --flake .#nixos-ssds --option extra-experimental-features "nix-command flakes"
+
+echo restarting main script
+sudo kill $(pgrep -f presentation.sh)
