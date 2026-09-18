8e0c9431baffffe22836355c3571afef7bce6e5e
Adam Fairbrother <adam.fairbrother@gmail.com>
2026-09-18 14:29:42 -0700

    Updated the updater script (So you can update what you update...)

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,12 @@ echo Pulling latest changes
 git pull
 
 echo Killing LibreOffice
-killall soffice.bin || true
+sudo kill $(pgrep -l soffice.bin)
+sudo kill $(pgrep -l libreoffice)
+sudo kill $(pgrep -l oosplash)
 
 echo Deploying system
 sudo nixos-rebuild switch --flake .#nixos-ssds --option extra-experimental-features "nix-command flakes"
+
+echo restarting main script
+sudo kill $(pgrep -l presentation.sh)
